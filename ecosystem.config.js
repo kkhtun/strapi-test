44b3262afdb40cd79bb6b81911d4cf395af8c8cd
kkhtun <khaingkhanthtun@gmail.com>
2022-08-29 20:52:05 +0630

    yarn

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "strapi-test", // Your project name
       cwd: "/home/ec2-user/strapi-test", // Path to your project
-      script: "npm", // For this example we're using npm, could also be yarn
+      script: "yarn", // For this example we're using npm, could also be yarn
       args: "start", // Script to start the Strapi server, `start` by default
       env: {
         NODE_ENV: "production",
