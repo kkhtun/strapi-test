70e937f460a130cdff253ea71c0508c10314a367
kkhtun <khaingkhanthtun@gmail.com>
2022-08-29 21:34:53 +0630

    test

--- config/env/production/server.ts
+++ config/env/production/server.ts
@@ -1,6 +1,6 @@
 export default ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
+  host: env("PROD_HOST", "0.0.0.0"),
+  port: env.int("PROD_PORT", 1337),
   app: {
     keys: env.array("APP_KEYS"),
   },
